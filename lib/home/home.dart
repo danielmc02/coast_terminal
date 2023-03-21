e83de706cfaf0602dcbe9c8fea8001b7ed139a4e
Daniel McCray <digity63@gmail.com>
2023-03-21 10:17:05 -0700

    Checkpoint

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,7 +1,9 @@
 
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:rive/rive.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -9,7 +11,24 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-      body: Container(color: Colors.red,)
+      body: Container(width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children:  [
+           const Spacer(flex: 10,),
+            //const SizedBox(width:300,height:300,child: RiveAnimation.asset(animations:,"./assets/rive_assets/movingplane.riv",fit: BoxFit.contain,)),
+            const Spacer(flex: 50,),
+            AnimatedTextKit(animatedTexts: [FadeAnimatedText("Loading anonymous token")]),
+            const Spacer(flex: 20,),
+            const LinearProgressIndicator(value: 40,),
+           const Spacer(flex: 10,)
+
+          ],
+      
+        ),
+      ),
     );
   }
 }
