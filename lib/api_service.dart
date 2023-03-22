45e15086d3e7c32db232aa807375cf3b5247c3ae
Daniel McCray <digity63@gmail.com>
2023-03-21 18:41:28 -0700

    Checkpoint

--- lib/api_service.dart
+++ lib/api_service.dart
@@ -1,14 +1,16 @@
+import 'dart:async';
+
 import 'package:firebase_auth/firebase_auth.dart';
 
-class ApiService
-{
+class ApiService {
   static ApiService? _instance;
-  late FirebaseAuth _auth;
+   FirebaseAuth? _auth;
+  User? _user;
 
   ApiService._internal() {
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-
-    
+    print("Created");
+    final FirebaseAuth _auth = FirebaseAuth.instance;
+   // final _user = _auth.currentUser;
   }
 
   static ApiService? get instance {
@@ -16,4 +18,38 @@ class ApiService
     return _instance;
   }
 
+  FirebaseAuth?  get auth
+  {
+    return _auth;
+  }
+  //Sign in anoynomously (guest)
+  Future signInAnon() async {
+    try {
+      final userCredential = await FirebaseAuth.instance.signInAnonymously();
+      print(userCredential.user);
+      //_user = _auth.currentUser;
+      print("Signed in with temporary account.");
+    } on FirebaseAuthException catch (e) {
+      switch (e.code) {
+        case "operation-not-allowed":
+          print("Anonymous auth hasn't been enabled for this project.");
+          break;
+        default:
+          print("Unknown error.");
+      }
+    }
+  }
+
+  Future deleteUser() async
+  {
+     
+
+      await FirebaseAuth.instance.currentUser!.delete();
+  }
+
+  Future signOut() async {
+    await FirebaseAuth.instance.signOut();
+  }
+
+  
 }
--- lib/loading_screen/loading_screen.dart
+++ lib/loading_screen/loading_screen.dart
@@ -1,32 +1,56 @@
 
 import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:coast_terminal/api_service.dart';
+import 'package:coast_terminal/loading_screen/provider/loading_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:provider/provider.dart';
 import 'package:rive/rive.dart';
 
-class LoadingScreen extends StatelessWidget {
+class LoadingScreen extends StatefulWidget {
   const LoadingScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return  Scaffold(
-      body: Container(width: MediaQuery.of(context).size.width,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisSize: MainAxisSize.max,
-          children:  [
-           const Spacer(flex: 10,),
-            //const SizedBox(width:300,height:300,child: RiveAnimation.asset(animations:,"./assets/rive_assets/movingplane.riv",fit: BoxFit.contain,)),
-            const Spacer(flex: 50,),
-            AnimatedTextKit(animatedTexts: [FadeAnimatedText("Loading anonymous token")]),
-            const Spacer(flex: 20,),
-            const LinearProgressIndicator(value: 40,),
-           const Spacer(flex: 10,)
+  State<LoadingScreen> createState() => _LoadingScreenState();
+}
 
-          ],
-      
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    print("Loading screen initialized");
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return  MultiProvider(
+      providers:[
+        ChangeNotifierProvider<LoadingProvider>(create: (_) => LoadingProvider()),
+      ] ,
+      child: Consumer<LoadingProvider>(
+        builder: (context, algo, child) => Scaffold(
+          body: Container(width: MediaQuery.of(context).size.width,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              children:  [
+               const Spacer(flex: 10,),
+                //const SizedBox(width:300,height:300,child: RiveAnimation.asset(animations:,"./assets/rive_assets/movingplane.riv",fit: BoxFit.contain,)),
+                const Spacer(flex: 50,),
+                AnimatedTextKit(animatedTexts: [FadeAnimatedText("Loading anonymous token")]),
+                const Spacer(flex: 20,),
+                const LinearProgressIndicator(value: 40,),
+               const Spacer(flex: 10,),
+               TextButton(onPressed: (){ApiService.instance!.deleteUser();}, child: Text("Delete account")),
+                         const Spacer(flex: 10,),
+                                      TextButton(onPressed: (){ApiService.instance!.signOut();}, child: Text("sign out account")),
+                         const Spacer(flex: 10,)
+
+              ],
+          
+            ),
+          ),
         ),
       ),
     );
