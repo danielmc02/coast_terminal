4b6f7150b237ddf393d3545b8bcb5890a7e2c330
Daniel McCray <digity63@gmail.com>
2023-04-17 10:53:29 -0700

    Checkpoint, need to fix transaction in home prov

--- lib/home/provider/home_provider.dart
+++ lib/home/provider/home_provider.dart
@@ -11,77 +11,57 @@ class HomeProvider extends ChangeNotifier {
   HomeProvider() {
     _init();
   }
-  _init() async
-  {
-    
-  }
+  _init() async {}
 
-  Future calculateIfThereAreMessages() async {
-    try
-    {
+  Future<MessageInstance?> calculateIfThereAreMessages() async {
+    print("this shouldn only print once");
+    try {
+      MessageInstance? fetchedMessage;
       //step 1: detect if and how many messages exist and plan accordingly via transactions
-   
-    int snapshot = await ApiService.instance!.getMessageCount();
-    if(snapshot > 1)
-    {
-      print('snapshot is greater than 0. This means there is message(s)');
-      /*
-int randomIntRange = (Random().nextDouble() * snapshot).floor();
-      print("Random intrange generated number is: $randomIntRange");
-            final query =  ApiService.instance!.messagesDatabase!.orderByKey().limitToFirst(0).startAt('$randomIntRange');
-      final messageSnapshot = await query.once();
-      messageSnapshot == null ? print("message snapshot null") : print("snapshot isnt null");
-      Map returnedMessage = messageSnapshot.snapshot.value as Map;
+      ApiService.instance!.messageCount!.runTransaction((currentCount)  {
+        int count = currentCount == null ? 0 : currentCount as int;
+        if (count == 0) {
+          print("Count is 0, there are no messages");
+          fetchedMessage = null;
+        } else if (count > 0) {
+          print(
+              "count is greater than 0, fetching a random message in process");
+          //just return the one and only message at messages node index 0
+          int randomInt =  Random().nextInt(count);
+          final queriedMessage = ApiService.instance!.messagesDatabase!
+              .orderByKey()
+              .limitToFirst(1)
+              .startAt(randomInt.toString())
+              .once()
+              .then((value) {
+            Map returnedMessage = value.snapshot.value as Map;
+            print(returnedMessage);
+            print(returnedMessage.entries.first.key);
+            print(returnedMessage.entries.first.value['Max Views']);
+            print(returnedMessage.entries.first.value['Badge Index']);
+            print(returnedMessage.entries.first.value['Message']);
+            print(returnedMessage.entries.first.value['Title']);
+            MessageInstance currentFetchedMessage = MessageInstance(
+                returnedMessage.entries.first.key,
+                returnedMessage.entries.first.value['Badge Index'],
+                returnedMessage.entries.first.value['Max Views'],
+                returnedMessage.entries.first.value['Title'],
+                returnedMessage.entries.first.value['Message']);
+            Boxes.getMessage().put('currentMessage', currentFetchedMessage);
+            fetchedMessage =  Boxes.getMessage().get('currentMessage');
+            print(fetchedMessage);
+          });
+          //  Map returnedMessage = queriedMessage.sna
 
-      print(returnedMessage);
-      print(returnedMessage.entries.first.key);
-      print(returnedMessage.entries.first.value['Max Views']);
-      print(returnedMessage.entries.first.value['Badge Index']);
-      print(returnedMessage.entries.first.value['Message']);
-      print(returnedMessage.entries.first.value['Title']);
+        }
+                  return Transaction.success(count);
 
-
-      
-      MessageInstance currentFetchedMessage = MessageInstance(returnedMessage.entries.first.key,returnedMessage.entries.first.value['Badge Index'],returnedMessage.entries.first.value['Max Views'],returnedMessage.entries.first.value['Title'],returnedMessage.entries.first.value['Message']);
-      Boxes.getMessage().put('currentMessage', currentFetchedMessage);
-      */
-      //query a message 
-    }
-    else if(snapshot == 1)
-    {
-      int indexOfFirstMessage = 0;
-      print('Snapshot claims therer is only 1 message, so I will retrieve the first message (at the zero index)');
-      print('The random int generated is 0 and always will be zero');
-      final query =  ApiService.instance!.messagesDatabase!.orderByKey().limitToFirst(1).startAt('$indexOfFirstMessage');
-      final messageSnapshot = await query.once();
-      messageSnapshot == null ? print("message snapshot null") : print("snapshot isnt null");
-      Map returnedMessage = messageSnapshot.snapshot.value as Map;
-/*
-      print(returnedMessage);
-      print(returnedMessage.entries.first.key);
-      print(returnedMessage.entries.first.value['Max Views']);
-      print(returnedMessage.entries.first.value['Badge Index']);
-      print(returnedMessage.entries.first.value['Message']);
-      print(returnedMessage.entries.first.value['Title']);
-*/
-
-      
-      MessageInstance currentFetchedMessage = MessageInstance(returnedMessage.entries.first.key,returnedMessage.entries.first.value['Badge Index'],returnedMessage.entries.first.value['Max Views'],returnedMessage.entries.first.value['Title'],returnedMessage.entries.first.value['Message']);
-      Boxes.getMessage().put('currentMessage', currentFetchedMessage);
-
-    }
-    else
-    {
-      print("There are 0 messages");
-    }
-
-
-    }
-    catch(e)
-    {
+      });
+      print("done");
+      return fetchedMessage;
+    } catch (e) {
       print(e);
     }
-  //  print("The number of messages in the db is: $snapshot");
-   return true;
+    //return true;
   }
 }
